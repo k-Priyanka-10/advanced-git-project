92eebb10c45a2d509cf82c8e31de840739265ec5
Priyanka <your_email@example.com>
2026-04-29 23:33:06 +0530

    Improved UI design

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 function addTask() {
-    let task = document.getElementById("taskInput").value;
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") return;
+
     let li = document.createElement("li");
     li.innerText = task;
 
     document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
 }
