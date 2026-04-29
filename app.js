c9a90b6467dafa8e5a38d8b4e4103f42804cbf91
Priyanka <your_email@example.com>
2026-04-29 22:54:11 +0530

    Added task manager feature

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-function showMessage() {
-    alert("App is working!");
+function addTask() {
+    let task = document.getElementById("taskInput").value;
+    let li = document.createElement("li");
+    li.innerText = task;
+
+    document.getElementById("taskList").appendChild(li);
 }
